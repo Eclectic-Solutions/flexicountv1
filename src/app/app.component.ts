5645f0ca7a4d76b9ad9354133d5051c6d377d8b3
Abhijit <info@eclecticsolutions.in>
2019-08-30 18:28:19 +0530

    push notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,286 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, AlertController, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Storage } from '@ionic/storage';
+
+
+
 import { HomePage } from '../pages/home/home';
+import { AlertPage } from '../pages/alert/alert';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage:any = HomePage;
+  keydeviceToken:string = 'deviceToken';
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private push: Push, private alertCtrl:AlertController, private http: Http, private storage: Storage) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      //statusBar.styleDefault();
+      
+      
       statusBar.styleDefault();
+      if(platform.is('android'))
+      {
+        statusBar.overlaysWebView(false);
+        statusBar.backgroundColorByHexString('#000000');
+      }
+      
+      
       splashScreen.hide();
+      
+      this.initPushNotification();
     });
   }
+  
+  
+  initPushNotification(){
+    
+    //check push notification permisson
+    
+    if (window.hasOwnProperty("cordova")) {
+      this.push.hasPermission().then((res: any) => {
+        if (res.isEnabled) {
+          console.log('We have permission to send push notifications');
+        } else {
+          console.log('We do NOT have permission to send push notifications');
+          
+          const alert = this.alertCtrl.create({
+            title: 'Notification Alert',
+            message: 'We do not have permission to send notification. Please enable notification.',
+            buttons: ['OK']
+          });        
+          alert.present();
+        }
+      });
+    }
+  
+  
+    const options: PushOptions = {
+        android: {
+          senderID: '112807975750'
+        },
+        ios: {
+            alert: 'true',
+            badge: true,
+            sound: 'false'
+        }
+    };
+
+    const pushObject: PushObject = this.push.init(options);
+    
+    pushObject.on('registration').subscribe((data: any) => {
+    
+      console.log('device token -> ' + data.registrationId);
+      
+      var headers = new Headers();
+      headers.append("Accept", 'application/json');
+      headers.append('Content-Type', 'application/json' );
+      const requestOptions = new RequestOptions({ headers: headers });
+      
+      let postData = {
+	"deviceToken": data.registrationId
+      }
+      
+      this.http.post("https://auth.biblicalarchaeology.org/pushnotification.php", postData, requestOptions)      
+      .subscribe(datalogin =>{
+	  console.log(datalogin);
+      }, error => {
+        console.log(error);	
+	
+      });      
+      
+      this.storage.set(this.keydeviceToken,data.registrationId);      
+      
+    });
+    
+    pushObject.on('notification').subscribe((data: any) => {    
+      
+      //start code to call MetricAlertMonitoringDeliver api
+      
+      this.storage.get('loginUserConfirmSiteURL').then((valLoginUserConfirmSiteURL) => {
+      
+        if(valLoginUserConfirmSiteURL)
+        {
+          this.storage.get('loginUserToken').then((valloginUserToken) => {
+          
+            if(valloginUserToken!='')
+            {      
+              var headers = new Headers();
+              headers.append("Authorization", 'Bearer '+valloginUserToken);       
+              const requestOptions = new RequestOptions({ headers: headers });
+              
+              let postData = {
+                "DomainID": data.DomainID,
+                "StoreID": data.StoreID,
+                "DepartmentID": data.DepartmentID
+              }
+              
+              this.http.post('https://'+valLoginUserConfirmSiteURL+'/api/Mobile/MetricAlertMonitoringDeliver',postData,requestOptions)
+              .map(res => res.json())
+              .subscribe(data =>{                            
+                console.log(data);
+              },err => {
+                console.log(err);
+              });
+            }
+            
+          });
+        }
+        
+      });
+      
+      //end code to call MetricAlertMonitoringDeliver api      
+      
+      console.log('message -> ' + data.message);      
+      if(data.additionalData.foreground)
+      { 
+        const alert = this.alertCtrl.create({
+          title: 'Notification',
+          message: data.message,
+          buttons:[
+            {
+              text:"OK",
+              handler:()=>
+              {
+                //this.nav.push(AlertPage);
+                
+                //code to check wheather sitelogin url is stored or not
+                
+                this.storage.get('loginUserConfirmSiteURL').then((valLoginUserConfirmSiteURL) => {
+                
+                  if(valLoginUserConfirmSiteURL)
+                  {
+                    //code to check user is logged in or not
+                    
+                    this.storage.get('loginUserToken').then((valloginUserToken) => {
+                    
+                      if(valloginUserToken!='')
+                      {
+                        //code to call MetricAlertMonitoringAcknowledge API
+                        
+                        var headers = new Headers();
+                        headers.append("Authorization", 'Bearer '+valloginUserToken);       
+                        const requestOptions = new RequestOptions({ headers: headers });
+                        
+                        let postData = {
+                          "DomainID": data.DomainID,
+                          "StoreID": data.StoreID,
+                          "DepartmentID": data.DepartmentID
+                        }
+                        
+                        this.http.post('https://'+valLoginUserConfirmSiteURL+'/api/Mobile/MetricAlertMonitoringAcknowledge',postData,requestOptions)
+                        .map(res => res.json())
+                        .subscribe(data =>{                            
+                            console.log(data);
+                        },err => {
+                            console.log(err);
+                        });
+                        
+                        
+                        this.nav.push(AlertPage);
+                      }
+                      else
+                      {
+                        this.nav.push(HomePage);
+                      }
+                    
+                    });
+                    
+                  }
+                  else
+                  {
+                    this.nav.push(HomePage);
+                  }                  
+                });
+                
+              }
+            }
+          ]
+        });        
+        alert.present();
+      }
+      else
+      {
+        //this.nav.push(AlertPage);
+        
+        //code to check wheather sitelogin url is stored or not
+        
+        this.storage.get('loginUserConfirmSiteURL').then((valLoginUserConfirmSiteURL) => {
+        
+          if(valLoginUserConfirmSiteURL)
+          {
+            //code to check user is logged in or not
+            this.storage.get('loginUserToken').then((valloginUserToken) => {
+            
+              if(valloginUserToken!='')
+              {
+                //code to call MetricAlertMonitoringAcknowledge API
+                
+                var headers = new Headers();
+                headers.append("Authorization", 'Bearer '+valloginUserToken);       
+                const requestOptions = new RequestOptions({ headers: headers });
+                
+                let postData = {
+                  "DomainID": data.DomainID,
+                  "StoreID": data.StoreID,
+                  "DepartmentID": data.DepartmentID
+                }
+                
+                this.http.post('https://'+valLoginUserConfirmSiteURL+'/api/Mobile/MetricAlertMonitoringAcknowledge',postData,requestOptions)
+                .map(res => res.json())
+                .subscribe(data =>{                    
+                    console.log(data);
+                },err => {
+                    console.log(err);
+                });
+                
+                
+                this.nav.push(AlertPage);
+              }
+              else
+              {
+                this.nav.push(HomePage);
+              }
+            
+            });
+            //this.nav.push(AlertPage);
+          }
+          else
+          {
+            this.nav.push(HomePage);
+          }                  
+        });
+      }
+    
+    });
+    
+    //pushObject.on('error').subscribe(error => console.error('Error with Push plugin' + error));
+    
+    pushObject.on('error').subscribe((error: any) => {
+    
+      console.log('Error with Push plugin -> ' + error);
+      
+      const alert = this.alertCtrl.create({
+        title: 'Device Token',
+        message: error.message,
+        buttons: ['OK']
+      });        
+      alert.present();
+      
+    });
+    
+  
+  }
+  
 }
 
